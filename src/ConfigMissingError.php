d95948196fc7a21000685cf2b6af2c4b4b66d769
linyows <linyows@gmail.com>
2022-07-31 01:16:50 +0900

    add error class

---
+++ src/ConfigMissingError.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ConfigMissingError
+ *
+ * @category Mailslot
+ * @package Mailslot
+ * @copyright  Copyright (c) 2022 Tomohisa Oda <linyows@gmail.com>
+ * @license MIT
+ */
+
+namespace Mailslot;
+
+class ConfigMissingError extends \Exception
+{
+}
